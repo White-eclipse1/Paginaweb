88fe1c649b61d859729ca4548f6de54e2224dcbc
Mauricio Figueroa <mafigue58@hotmail.com>
2025-11-04 17:26:41 -0600

    Decap

--- api/auth/index.js
+++ api/auth/index.js
@@ -1,8 +1,15 @@
 export default async function (context, req) {
   const clientId = process.env.OAUTH_CLIENT_ID;
   const scope = "repo,user:email";
-  const host = process.env.SWA_HOSTNAME || req.headers["x-forwarded-host"];
-  const base = `https://${host}`;
+
+  // Toma hostname de var de entorno o del header
+  const hostFromEnvOrHeader = process.env.SWA_HOSTNAME || req.headers["x-forwarded-host"];
+
+  // Si ya empieza con http, úsalo tal cual; si no, antepone https://
+  const base = hostFromEnvOrHeader.startsWith("http")
+    ? hostFromEnvOrHeader
+    : `https://${hostFromEnvOrHeader}`;
+
   const redirectUri = `${base}/api/callback`;
 
   const url = new URL("https://github.com/login/oauth/authorize");
